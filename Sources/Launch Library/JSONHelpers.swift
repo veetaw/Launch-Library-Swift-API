b1698314be1b1c439b93a45b6d251df4c4e198fb
Vito <vitopiegari01@gmail.com>
2021-01-26 11:24:50 +0100

    Initial library skeleton

---
+++ Sources/Launch Library/JSONHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vito Piegari on 12/01/21.
+//
+
+import Foundation
---
+++ Sources/Launch Library/Models/Rocket.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vito Piegari on 11/01/21.
+//
+
+import Foundation
